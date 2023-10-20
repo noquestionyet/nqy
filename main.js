9112bc947f8ea258e2c742e57ad545d281dfc1bb
Polly <chekhonina.polina@gmail.com>
2023-10-20 15:48:37 +0200

    #test

--- main.js
+++ main.js
@@ -680,6 +680,7 @@ if (document.querySelector('[nqy-quiz="submit"]')) {
 
 // sending the user results to the db
 function sendPoints (userName, userEmail, quizName, totalPoints, userAnswers, currentUserId) {
+  console.log(userAnswers)
   const quizNameUpdated = quizName.replace(/\W+/g, '_').toLowerCase();
   const finalData = {
     total_points: Number(totalPoints),
